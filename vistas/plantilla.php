e1c8cff1c3ecbe54d5128e9c2949f0aa210ac332
bunker <bunker9dev@gmail.com>
2024-05-28 16:35:36 -0500

    cabezote listo y funcionando

---
+++ vistas/plantilla.php
@@ -0,0 +1,129 @@
+<?php
+
+// session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+  <title>Sistema de Inventario A2</title>
+  <link rel="icon" href="vistas/img/plantilla/1-icon-inventario-sia2.svg">
+
+
+  <!-- ============================================
+PLUGLIN CSS
+==============================================-->
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="vistas/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="vistas/dist/css/adminlte.css">
+</head>
+
+
+
+ <!-- ============================================
+PLUGLIN JAVASCRIPT
+==============================================-->
+
+  <!-- jQuery -->
+  <script src="vistas/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="vistas/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="vistas/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+
+
+
+  <body class="hold-transition sidebar-mini">
+<!-- <body class="hold-transition skin-blue sidebar-collapse sidebar-mini wrapper"> -->
+<!-- login-page -->
+
+<div class="wrapper">
+
+
+
+  <?php
+
+  // if (isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok") {
+
+
+  //   echo '<div class="wrapper">';
+
+
+  //   // CABEZOTE
+    include "modulos/cabezote.php";
+
+  //   // MENU
+    include "modulos/menu.php";
+
+  //   // CONTENIDO
+  //   if (isset($_GET["ruta"])) {
+  //     if (
+  //       $_GET["ruta"] == "inicio" ||
+  //       $_GET["ruta"] == "dashboard-v1" ||
+  //       $_GET["ruta"] == "dashboard-v2" ||
+  //       $_GET["ruta"] == "cliente-crear" ||
+  //       $_GET["ruta"] == "cliente-consultar" ||
+  //       $_GET["ruta"] == "producto-crear" ||
+  //       $_GET["ruta"] == "producto-consultar" ||
+  //       $_GET["ruta"] == "inventario-entradas" ||
+  //       $_GET["ruta"] == "inventario-salidas" ||
+  //       $_GET["ruta"] == "configuracion" ||
+  //       $_GET["ruta"] == "usuarios" ||
+  //       $_GET["ruta"] == "reportes" ||
+  //       $_GET["ruta"] == "salir") {
+
+  //       include "modulos/" .$_GET["ruta"]. ".php";
+
+  //     } else {
+
+  //       include "modulos/404.php";
+  //     }
+
+  //   } else {
+
+  //     include "modulos/inicio.php";
+  //   }
+
+
+
+
+    // FOOTER
+    include "modulos/footer.php";
+
+
+
+  // } else {
+
+  //   include "modulos/login.php";
+  // }
+
+
+  ?>
+
+
+
+
+</div>
+
+
+
+ 
+  <!-- <script src="vistas/dist/js/demo.js"></script> -->
+
+  <script src="js/plantilla.js"></script>
+
+
+</body>
+
+</html>
