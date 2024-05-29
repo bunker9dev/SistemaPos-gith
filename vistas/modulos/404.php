16a9cfd049899964bebd6229cec0594a4c04537f
bunker <bunker9dev@gmail.com>
2024-05-28 22:46:15 -0500

    pagina 404 ok

---
+++ vistas/modulos/404.php
@@ -0,0 +1,42 @@
+<div class="content-wrapper">
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+
+                <div class="col-sm-6">
+                    <h1>Página no encontrada</h1>
+                    
+                </div>
+
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
+                        <li class="breadcrumb-item active">Págia no encontrada</li>
+                    </ol>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="error-page">
+            <h2 class="headline text-warning">404</h2>
+
+            <div class="error-content">
+            <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Página no encontrada.</h3>
+
+            <p>
+            Ingrese al menú lateral y allí podrás encontrar las páginas disponibles. Tambien puedes regresar haciendo <a href="inicio">click aquí</a> 
+          </p>
+
+            </div>
+        </div>
+
+        
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
